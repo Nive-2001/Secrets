f22b4c37d2ec532cb0a14de10bc22c2f23b61a5d
Nivethitha A S <nivethithaprema@gmail.com>
2021-07-17 23:44:46 +0530

    used md5

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 const app = express();
 
@@ -21,7 +21,6 @@ const userShema = new mongoose.Schema({
 });
 
 
-userShema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userShema);
 
@@ -40,7 +39,7 @@ app.get("/register", function(req, res) {
 app.post("/register", function(req, res) {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save(function(err) {
